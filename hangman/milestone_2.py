23564dd7f9f3558919489d075f4528a199fd8cce
acq3047 <acq3047@gmail.com>
2024-05-17 12:12:14 +0100

    Add some coments in the milestone_2.py

--- hangman/milestone_2.py
+++ hangman/milestone_2.py
@@ -4,19 +4,23 @@
 import random
 
 # Task 1: Define the list of possible words
+
 fruit_list = ['apple', 'strawberry', 'grape', 'orange', 'pinaple'] # List of my 5 favourite fruits
 word_list = fruit_list
 print(word_list)
 
 # Task 2: Choose a random word from the list
 
-choice = random.choice(word_list)
+choice = random.choice(word_list) # We use the choice method to allos the computer to randomly choose between the words in the list
 print(choice)
 
 # Task 3: Ask the user for an input
+
 guess = input('Enter a single letter: ')
 
-if len(guess) == 1:
+# Task 4: Check out that the input is not a single character
+
+if len(guess) == 1: # Check that the input has only contains one character
     print('Good guess!')
 else:
     print('Oops! That is not a valid input')
